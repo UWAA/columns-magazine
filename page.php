b1324f21b4873a02261e5db3bcbbbb4df84e2139
Ben Erickson <bperick@uw.edu>
2016-12-27 09:11:31 -0800

    page template for static Columns pages (about/add/etc)

---
+++ page.php
@@ -0,0 +1,51 @@
+<?php 
+get_header("columns");
+use \Columns\Utilities as Utilities;
+
+?>
+<div class="single-header">    
+</div>
+
+<div class="container-fluid">
+
+    <div class="row">
+        <div class="breadcrumbs">
+            <?php uw_breadcrumbs() ?>
+        </div> 
+        <div class="content-tags">
+            <!-- Fluid containter, 2 cols -->
+        </div>
+    </div>
+
+    <div class="columns-feature-content">
+        <div class="row">
+            <?php
+
+
+            while ( have_posts() ) : the_post(); 
+
+            ?>
+
+            <h1><?php the_title() ?></h1>
+
+                <?php the_content(); ?>           
+
+
+
+            <?php
+
+            endwhile;
+
+            ?>
+
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php
+
+
+get_footer(); 
