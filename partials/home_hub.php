c44e4e634894a2992905e90c3ff96bba12bbe10e
Ben Erickson <bperick@uw.edu>
2016-12-28 16:50:41 -0800

    temporary modal close

--- partials/home_hub.php
+++ partials/home_hub.php
@@ -71,7 +71,8 @@ $args = array (
       <div class="modal-container modal fade" id="HUB_Modal">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
-            <div class="modal-body">              
+            <div class="modal-body">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
               <div class="gallery" data-flickity='{ "cellAlign": "left", "contain": true, "cellSelector" : ".hub_modal_gallery_item" }'>
 
 
