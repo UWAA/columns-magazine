b4bccbd49f50c34a2028ed09fc52bb35803c73b9
Ben Erickson <bperick@uw.edu>
2017-06-05 15:35:30 -0700

    v1 social share complete

--- partials/social_share.php
+++ partials/social_share.php
@@ -1,17 +1,38 @@
 <?php
+//Twitter Button
+$twitterIntentBaseURL = 'https://twitter.com/intent/tweet';
 $twitterIntentExcerpt = '?text=' . urlencode(get_the_excerpt());
 $twitterIntentURL = '&url=' . urlencode(get_the_permalink());
-$twitterIntentBaseURL = 'https://twitter.com/intent/tweet';
+
 
 $compiledTwitterIntentURL = $twitterIntentBaseURL . $twitterIntentExcerpt . $twitterIntentURL; 
+
+
+// Email Link
+$emailSubject = '&subject=Columns Magazine -- ' . get_the_title();
+$bodyCopy = "I thought you would like this article on Columns Magazine:  " . get_the_permalink() ;
+$emailBody = '?body=' . $bodyCopy;
+
+$compiledMailLink = "mailto:" . $emailBody . $emailSubject;
+
+// LinkedIn URL
+$linkedInShareBaseURL = "https://www.linkedin.com/shareArticle?mini=true";
+$linkedInURL = $twitterIntentURL; //same as Twitter, starts with and
+$linkedInTitle = '&title=' . get_the_title();
+$linkedInSummary = '&summary=' .urlencode(get_the_excerpt());
+
+$compiledLinkedInLink = $linkedInShareBaseURL . $linkedInURL . $linkedInTitle . $linkedInSummary;
+
+
+;
  ?>
 <div class="social-share-container">
     <div class="row">
     <ul class="social-share">
                     <li><a href="#" id="shareBtn" class="facebook"><span class="offscreen">Columns Magazine Facebook</span></a></li>
-                    <li><a href="<?php echo $compiledTwitterIntentURL ?>" class="twitter"><span class="offscreen">@ColumnsMag</span></a></li>
-                    <li><a href="#" class="linkedin"><span class="offscreen">@ColumnsMag</span></a></li>
-                    <li><a href="#" class="email"><span class="offscreen">@ColumnsMag</span></a></li>                    
+                    <li><a href="<?php echo $compiledTwitterIntentURL ?>" target="_blank" class="twitter"><span class="offscreen">@ColumnsMag</span></a></li>
+                    <li><a href="<?php echo $compiledLinkedInLink ?>" target="_blank" class="linkedin"><span class="offscreen">@ColumnsMag</span></a></li>
+                    <li><a href="<?php echo $compiledMailLink ?>" class="email"><span class="offscreen">@ColumnsMag</span></a></li>                    
     </ul>
     </div>
     <hr>
