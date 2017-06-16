454e933e494fe0344b0027251449534fe65f3b65
Ben Erickson <bperick@uw.edu>
2017-06-16 10:47:44 -0700

    WIP, all elements in

--- js/_04search.js
+++ js/_04search.js
@@ -2,12 +2,20 @@ jQuery(document).ready(function($) {
 
   $('#icon-search').click(function(event) {
     var $width = $( window ).width();
-      if ($width < 425) {
+      if ($width < 479) {
         $('.search-mobile').toggle();
       } else {
         $('.search-widescreen')
           .toggle();
       }    
+    $(this).toggleClass('is-active');
+  });
+
+  $('#navbar-button').click(function(event) {
+    var $width = $( window ).width();
+      if ($width < 768) {
+        $('.menu-columns-navigation-container').toggleClass('show');
+      }  
     $(this).toggleClass('is-active');
   });
 
