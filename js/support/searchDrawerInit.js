8cb0ea695c5e5e8dfd8ee3080fc172bb0567f3ad
Ben Erickson <bperick@uw.edu>
2018-06-29 15:49:33 -0700

    android scroll

--- js/support/searchDrawerInit.js
+++ js/support/searchDrawerInit.js
@@ -21,8 +21,8 @@ $('.drawer').drawer({
     iscroll: {
         // Configuring the iScroll
         // https://github.com/cubiq/iscroll#configuring-the-iscroll
-        mouseWheel: true,
-        preventDefault: false
+        mouseWheel: false,
+        preventDefault: true
         
         
     },
